8cae4eb8a5d071c991fe53f6bec8fb878d8c5428
Ramona <Amaryllisaea@gmail.com>
2025-03-23 01:56:01 +0100

    Update scripts2.js

--- scripts2.js
+++ scripts2.js
@@ -1,7 +1,7 @@
 // Seem to work a lot faster, but did not really check if it gives rewards
 document.querySelectorAll("video").forEach(v => {
   const interval = setInterval(() => {
-    v.currentTime += 2;
+    v.currentTime += 10;
     if (v.currentTime >= v.duration) {
       clearInterval(interval);
     }
